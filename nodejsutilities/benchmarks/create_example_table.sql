7fbb3a63dfd4f7415972df8de289a3fce235a269
manuel.depaz <meusebio.depaz@bbva.com>
2013-11-03 22:44:17 +0100

    duplicated file

--- nodejsutilities/benchmarks/create_example_table.sql
+++
@@ -1,6 +0,0 @@
-create database node_test;
-use node_test;
-CREATE TABLE IF NOT EXISTS `example` (
-  `id` int(11) DEFAULT NULL,
-  `data` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
