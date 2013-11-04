262ce8c98b271ca5424d796effc54060dfb3d5fb
carlos <carlos@beeva.(none)>
2013-11-04 14:35:22 +0100

    changes in parse_json_lines.js (it takes the json from the stdin)

--- P02_nodejsutilities/parse_json_lines.js
+++ P02_nodejsutilities/parse_json_lines.js
@@ -1,20 +1,10 @@
-if(process.argv.length<3){
-	console.error("missing argument (file to parse)");
-	return(-1);
-}
-
-var fs = require('fs'),readline = require('readline');
-
-var rd = readline.createInterface({
-    input: fs.createReadStream(process.argv[2]),
-    output: process.stdout,
-    terminal: false
-});
-
-rd.on('line', function(line) {
-    try{
-	var l=JSON.parse(line);
-	console.log(JSON.stringify(l,null,4));
-    }catch(error){}
-    
-});
+var Lazy=require("lazy");
+new Lazy(process.stdin).lines.forEach(
+          function(line) { 
+    		try{
+			var l=JSON.parse(line);
+			console.log(JSON.stringify(l,null,4));
+    		}catch(error){}
+          }
+);
+process.stdin.resume();
