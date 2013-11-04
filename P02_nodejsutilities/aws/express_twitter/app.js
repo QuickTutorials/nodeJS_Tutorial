74e6423719b0430ddd0efd100a471a5b3dbe57b4
Manuel Eusebio de Paz Carmona <manuel.depaz@beeva.com>
2013-11-04 12:59:55 +0100

    Updating use of private file

--- P02_nodejsutilities/aws/express_twitter/app.js
+++ P02_nodejsutilities/aws/express_twitter/app.js
@@ -1,19 +1,25 @@
-var app = require('express').createServer(),
-    twitter = require('ntwitter');
+var app = require('express').createServer(), twitter = require('ntwitter');
 
 app.listen(3000);
 
-var twit = new twitter({
-  consumer_key: 'aaaaaaadsfasdf',
-  consumer_secret: 'adsfasdfañlkjasfdlkjasdf',
-  access_token_key: 'adsfasdfasdff',
-  access_token_secret: 'asdfadsfasdfasdfasdf'
-});
+var credentials = require('./privateCredentials.json');
 
-//twit.stream('statuses/filter', { track: ['love', 'hate'] }, function(stream) {
-twit.stream('statuses/filter', { track: ['bbva'] }, function(stream) {
-  stream.on('data', function (data) {
-    console.log(JSON.stringify(data));
-    console.log(",")
-  });
+/*
+ * Saved in a json private file 
+	{
+	"consumer_key": "....",
+	"consumer_secret": "....",
+	"access_token_key": "......",
+	"access_token_secret": "...."
+	}
+*/
+
+var twit = new twitter(credentials);
+
+//twit.stream('statuses/filter', { track : [ 'love', 'hate' ] }, function(stream) {		// a lot of tweets!!
+twit.stream('statuses/filter', { track: ['cristiano ronaldo'] }, function(stream) {
+	stream.on('data', function(data) {
+		console.log(JSON.stringify(data));
+		console.log(",")
+	});
 });
